05d04a060082fb51b1a34c6caf81f4c9ff1d855e
Mira L <30845365+miramliu@users.noreply.github.com>
2025-02-01 22:39:12 -0500

    Update Algorithm2.m
    Updated Algorithm2, originally from M.S. Shazeeb. Kept main fit aspect, changed variable names for consistency, made it a voxel-wise analysis rather than map-wise analysis.

--- Algorithm2.m
+++ Algorithm2.m
@@ -1,4 +1,8 @@
- for slice_ct=11 %1:25
+
+function Output = Algorithm2(bvalues, signal)
+
+%{
+for slice_ct=11 %1:25
         for i=1:max
             for j=1:max
                 for c=1:5
@@ -7,45 +11,60 @@
                         vec(c)=0;
                     end
                 end
+%}
+    vec = log(signal/signal(1)); % ln(S/S0) normalize all signal
+    vec=vec(:); %force to be column vector
+  
+    bvalues = bvalues(:);
+    N_bvalues=length(bvalues);
+    b_split = 6; %the threshold of bvalues where b>bvalue(blim) is slow only.
 
-                %generate D map
-                [mono_fitresult, gof] = fit(b_D',vec',ft_mono);
-                D_mat(i,j,slice_ct) = -mono_fitresult.p1;
-                for d=1:10
-                    norm_vec(d)=slice(i,j,d,slice_ct)/slice(i,j,b_0,slice_ct); %S/S0 from b=0-900
-                    if isnan(norm_vec(d))||norm_vec(d)==Inf||norm_vec(d)==-Inf
-                        norm_vec(d)=0;
-                    end
+
+    %generate D map
+    %[mono_fitresult, gof] = fit(b_D',vec',ft_mono);
+    [mono_fitresult, ~] = fit(bvalues(b_split:N_bvalues),vec(b_split:N_bvalues),'ax+b');
+    D_fit = -mono_fitresult.p1;
+
+
+    %generate D* and f map
+    ft_bi = fittype('(1-f)*exp(-x*D)+f*exp(-x*(Dstar))','dependent',{'y'},'independent',{'x'},'problem',{'D'},'coefficients',{'Dstar','f'});
+    fo_bi = [0.0004, 0.005, 0.07]; %startpoints, in alphabetical order, so D, Dstar, f. Check Algorithm 1 startpoint order!
+    try
+        [fitmod_bi,good_bi,~]=fit(bvalues,signal(:)/signal(1),ft_bi,fo_bi, 'problem', D_fit); %no longer in log space
+        Output.D=D_fit;
+        Output.Dstar=fitmod_bi.Dstar;
+        Output.f = fitmod_bi.f;
+        Output.RSSE = good_bi.adjrsquare;
+    catch
+        Output.D=0;
+        Output.Dstar=0;
+        Output.f = 0;
+        Output.RSSE = NaN;
+    end
+                    %{
+%                     if good_bi.adjrsquare < 0.80
+%                         Dstar_mat(i,j,slice_ct) = 0;
+%                         f_mat(i,j,slice_ct) = 0;
+%                     else
+                        Dstar_mat(i,j,slice_ct) = fitmod_bi.Dstar;
+                        f_mat(i,j,slice_ct) = fitmod_bi.f;
+                        good_bi_fit(i,j,slice_ct) = good_bi.adjrsquare;
+                        
+%                     end
+                catch
+                    error = error + 1;
                 end
                 
+                raw_counter=raw_counter+1;
+                raw_normvec(raw_counter,:)=norm_vec;
+                for d=1:10
+                    bifit_raw_normvec(raw_counter,d)=(1-fitmod_bi.f)*exp(-b_val(d)*fitmod_bi.D)+fitmod_bi.f*exp(-b_val(d)*fitmod_bi.Dstar);
+                end
 
-    %            [maxi,loc]=max(norm_vec);
-    %             if maxi==1 && loc==1
-                    curr_D=D_mat(i,j,slice_ct);
-                    %generate D* and f map
-                    ft_bi = fittype('(1-f)*exp(-x*D)+f*exp(-x*(Dstar))','dependent',{'y'},'independent',{'x'},'problem',{'D'},'coefficients',{'Dstar','f'});
-                    try
-                        [fitmod_bi,good_bi,out_bi]=fit(b_val',norm_vec',ft_bi,fo_bi, 'problem', curr_D);
-    %                     if good_bi.adjrsquare < 0.80
-    %                         Dstar_mat(i,j,slice_ct) = 0;
-    %                         f_mat(i,j,slice_ct) = 0;
-    %                     else
-                            Dstar_mat(i,j,slice_ct) = fitmod_bi.Dstar;
-                            f_mat(i,j,slice_ct) = fitmod_bi.f;
-                            good_bi_fit(i,j,slice_ct) = good_bi.adjrsquare;
-    %                     end
-                    catch
-                        error = error + 1;
-                    end
-                    
-                    raw_counter=raw_counter+1;
-                    raw_normvec(raw_counter,:)=norm_vec;
-                    for d=1:10
-                        bifit_raw_normvec(raw_counter,d)=(1-fitmod_bi.f)*exp(-b_val(d)*fitmod_bi.D)+fitmod_bi.f*exp(-b_val(d)*fitmod_bi.Dstar);
-                    end
-    %            else
-    %                 Dstar_mat(i,j,slice_ct) = 0;
-    %                 f_mat(i,j,slice_ct) = 0;
-    %             end
-            end
+%            else
+%                 Dstar_mat(i,j,slice_ct) = 0;
+%                 f_mat(i,j,slice_ct) = 0;
+%             end
         end
+        end
+                %}
