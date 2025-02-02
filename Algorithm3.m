8178184e4ef141701194f871bd3187bb5ad0c1e7
Mira L <30845365+miramliu@users.noreply.github.com>
2025-02-01 22:39:47 -0500

    Update Algorithm3.m
    Updated algorithm3 originally from MS Shazeeb. Updated variable names for consistency and changed to voxel-wise analysis. Assumed new input start values.

--- Algorithm3.m
+++ Algorithm3.m
@@ -1,4 +1,41 @@
-    for slice_ct=11 %1:25
+    
+function Output = Algorithm3(bvalues,signal)
+
+
+    vec = log(signal/signal(1)); % ln(S/S0) normalize all signal
+    vec=vec(:); %force to be column vector
+  
+    bvalues = bvalues(:);
+    N_bvalues=length(bvalues);
+    b_split = 6; %the threshold of bvalues where b>bvalue(blim) is slow only.
+
+
+    %generate D map
+    %[mono_fitresult, gof] = fit(b_D',vec',ft_mono);
+    [mono_fitresult, ~] = fit(bvalues(b_split:N_bvalues),vec(b_split:N_bvalues),'ax+b');
+    D_fit = -mono_fitresult.p1;
+    f0_fit = 1-exp(mono_fitresult.p2);
+
+
+    %generate D* and f map
+    ft_bi = fittype('(1-f0)*exp(-x*D)+f*exp(-x*(Dstar))','dependent',{'y'},'independent',{'x'},'problem',{'D','f0'},'coefficients',{'Dstar','f'});
+    fo_bi = [0.0004, 0.005, 0.07];
+    try
+        [fitmod_bi,good_bi,~]=fit(bvalues,signal(:)/signal(1),ft_bi,fo_bi, 'problem', {D_fit f0_fit});
+        Output.D=D_fit;
+        Output.Dstar=fitmod_bi.Dstar;
+        Output.f = fitmod_bi.f; % note that while this does give f0, it also returns an f (?) 
+        Output.RSSE = good_bi.adjrsquare;
+    catch
+        Output.D=0;
+        Output.Dstar=0;
+        Output.f = 0;
+        Output.RSSE = NaN;
+    end
+end
+    %{
+%{
+for slice_ct=11 %1:25
         for i=1:max
             for j=1:max
                 for c=1:5
@@ -19,23 +56,16 @@
                     end
                 end
                 
-
-    %            [maxi,loc]=max(norm_vec);
-    %             if maxi==1 && loc==1
-                    curr_D=D_mat(i,j,slice_ct);
-                    curr_f0 = f0_mat(i,j,slice_ct);
-                    %generate D* and f map
-                    ft_bi = fittype('(1-f0)*exp(-x*D)+f*exp(-x*(Dstar))','dependent',{'y'},'independent',{'x'},'problem',{'D','f0'},'coefficients',{'Dstar','f'});
-                    try
-                        [fitmod_bi,good_bi,out_bi]=fit(b_val',norm_vec',ft_bi,fo_bi, 'problem', {curr_D curr_f0});
-    %                     if good_bi.adjrsquare < 0.80
-    %                         Dstar_mat(i,j,slice_ct) = 0;
-    %                         f_mat(i,j,slice_ct) = 0;
-    %                     else
+%}
+     
+                           if good_bi.adjrsquare < 0.80
+                             Dstar_mat(i,j,slice_ct) = 0;
+                             f_mat(i,j,slice_ct) = 0;
+                         else
                             Dstar_mat(i,j,slice_ct) = fitmod_bi.Dstar;
                             f_mat(i,j,slice_ct) = fitmod_bi.f;
                             good_bi_fit(i,j,slice_ct) = good_bi.adjrsquare;
-    %                     end
+                         end
                     catch
                         error = error + 1;
                     end
@@ -45,9 +75,10 @@
                     for d=1:10
                         bifit_raw_normvec(raw_counter,d)=(1-fitmod_bi.f0)*exp(-b_val(d)*fitmod_bi.D)+fitmod_bi.f*exp(-b_val(d)*fitmod_bi.Dstar);
                     end
-    %            else
-    %                 Dstar_mat(i,j,slice_ct) = 0;
-    %                 f_mat(i,j,slice_ct) = 0;
-    %             end
+                else
+                     Dstar_mat(i,j,slice_ct) = 0;
+                     f_mat(i,j,slice_ct) = 0;
+                 end
             end
         end
+    %}
